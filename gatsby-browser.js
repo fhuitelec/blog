7c47c6d88ed8e6580d86b6b5d5d96afee2b0741b
Fabien Huitelec <fabien@huitelec.fr>
2020-05-16 22:54:50 +0200

    feat: syntax highlight tweaking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,7 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "prismjs/themes/prism.css"
+// import "prismjs/themes/prism-okaidia.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+import "./src/styles/global.css"
+import "./src/styles/darcula.css"
