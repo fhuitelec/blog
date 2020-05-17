d1523a68e58e67f507bbf5fbadd16ce798fa156f
Fabien Huitelec <fabien@huitelec.fr>
2020-05-17 16:03:30 +0200

    refactor: split global CSS files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,11 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+// Libraries
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
-// import "prismjs/themes/prism-okaidia.css"
-import "prismjs/plugins/line-numbers/prism-line-numbers.css"
-import "./src/styles/global.css"
-import "./src/styles/darcula.css"
+// Fonts
+import "./src/styles/fonts/jetbrains-mono.css"
+
+// Local
+import "./src/styles/gatsby-code-file-name.css"
+import "./src/styles/key.css"
+import "./src/styles/gatsby-code-highlight-fixes.css"
+import "./src/styles/code-highlight-darcula.css"
