4efcb4305a95db0037ae40af34c0f1fa645a4903
Fabien Huitelec <fabien@huitelec.fr>
2024-01-10 12:09:17 +0100

    feat: enable Plausible plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,5 +134,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-plausible`,
+      options: {
+        domain: `blog.fabien.sh`,
+      },
+    },
   ],
 }
